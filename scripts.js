4d2210ffabe15cb6ca57fcfd9379431aad29ecce
Vito Di Grigoli <73437231+vitodigrigoli@users.noreply.github.com>
2024-04-08 20:04:44 +0200

    footer, cards, packets, animation

--- scripts.js
+++ scripts.js
@@ -1,15 +1,26 @@
-const swiper = new Swiper(".swiper", {
-	effect: "cards",
-	grabCursor: true,
-	centeredSlides: true,
-	loop: true,
-	slidesPerView: 1.5,
-	pagination: {
-	  el: ".swiper-pagination",
-	},
-	breakpoints: {
-		1024: {
-			slidesPerView: 3,
-			}
+const  iOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+
+if (iOS) {
+    document.addEventListener("DOMContentLoaded", function() {
+        var parallaxElements = document.querySelectorAll(".parallax");
+        parallaxElements.forEach(function(el) {
+            el.style.backgroundAttachment = "scroll";
+            // Add more rules here if needed
+        });
+    });
+}
+
+const watch = document.querySelectorAll('.watch')
+const intersection_observer = new IntersectionObserver( (items) =>{
+	items.forEach( item => {
+		
+		if( item.isIntersecting){
+			item.target.classList.add('in-view')
 		}
-});
+		else{
+			item.target.classList.remove('in-view')
+		}
+	})
+}, { threshold: 0.1} )
+
+watch.forEach( el => intersection_observer.observe(el))
